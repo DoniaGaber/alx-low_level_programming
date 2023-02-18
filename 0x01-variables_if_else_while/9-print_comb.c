df066854e9ff4fcdc5e735cb4135788d5daffc3e
DoniaGaber <doniagaber797@gmail.com>
2023-02-18 15:14:31 -0800

    task 9-print_como.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* Write a program that prints combinations of single-digit numbers. */
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+  int i;
+  /* for loop. */
+  for (i = 48; i < 58; i++)
+    {
+      putchar(i);
+      if (i != 57)
+	{
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
+  return (0);
+}
