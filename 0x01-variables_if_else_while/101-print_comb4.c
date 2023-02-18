d4904e693cf5bb0ce851b823dccf5f0d02cfdfde
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-18 15:26:45 -0800

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,38 @@
-#include <stdlib.h>
 #include <stdio.h>
-/* Program prints all possible combinations of two two-digit numbers */
 
 /**
- * main - create 2 pairs of numbers that do not repeat
- * Return: 0
- */
+ ** main - print all possible combination of two-digit numbers
+ **
+ ** Return: Always 0 (success)
+ **/
+
 int main(void)
 {
-	int j, i;
-
-	/* for loop */
-	for (j = 0; j <= 99; j++)
-	{
-		for (i = j; i <= 99; i++)
-		{
-			if (i != j)
-			{
-				putchar(j / 10 + 48);
-				putchar(j % 10 + 48);
-				putchar(' ');
-				putchar(i / 10 + 48);
-				putchar(i % 10 + 48);
-
-				if (j * 100 + i != 9899)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int i = 0;
+int j = 1;
+while (i <= 98)
+{
+while (j <= 99)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i == 98 && j == 99)
+{
+putchar('\n');
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+j = i + 1;
+}
+return (0);
 }
