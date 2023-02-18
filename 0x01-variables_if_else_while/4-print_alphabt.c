ef5c3b720fd38dc74133e43c0d63fa0b06f5d0e6
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-18 14:25:40 -0800

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,28 @@
-task
+#include <stdlib.h>
+#include <stdio.h>
+/* Write a program that prints the alphabet in lowercase */
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char b;
+	/* for loop */
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		if (b != 'q' && b != 'e')
+		{
+            b++; continue;
+		    
+		}
+		else{
+		    putchar(b); 
+		}
+			
+	    
+	}
+	putchar('\n');
+	return (0);
+}
