0f4dd34ac8f0f6ea7777c30ed6b62c84cdedbce8
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-18 15:02:37 -0800

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-  /*while loop */
-  int x = '0';
-    while (x <= '9')
-    {
-        putchar(x);
-        x++;
-    }
-	/* for loop */
-  for( x = 'a'; x <= 'f'; x++){
-    putchar(x);
-  }
-  putchar('\n');
-	return (0);
+/*while loop */
+int x = '0';
+while (x <= '9')
+{
+putchar(x);
+x++;
+}
+/* for loop */
+for( x = 'a'; x <= 'f'; x++){
+putchar(x);
+}
+putchar('\n');
+return (0);
 }
