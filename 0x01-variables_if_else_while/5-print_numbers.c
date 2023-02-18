d0fc0e358325070df115ba6400ba0c4fe6c33ed5
DoniaGaber <doniagaber797@gmail.com>
2023-02-18 14:32:49 -0800

    task 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* program that prints all single digit numbers of base 10 starting from 0 */
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+  int a;
+  /* for loop */
+  for (a = 0; a < 10; a++)
+    {
+      printf("%i", a); }
+  printf("\n");
+  return (0);
+}
