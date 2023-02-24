525e36c0f9c7b32bc7a2b58d92c22222fd156036
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-23 16:33:25 -0800

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,14 +10,14 @@
 
 void print_most_numbers(void)
 {
-  int number;
+	int number;
 
-  for (number = '0'; number <= '9'; number++)
-    {
-      if (number == '2' || number == '4')
-	;
-      else
-	_putchar(number);
-    }
-  _putchar('\n');
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
+	}
+	_putchar('\n');
 }
