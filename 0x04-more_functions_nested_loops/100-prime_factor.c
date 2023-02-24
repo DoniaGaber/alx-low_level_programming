19a61712402063cacf3bdea425d62dcfd834ba5d
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-23 17:09:01 -0800

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  unsigned long int prime = 612852475143;
-  unsigned long int i = 3;
+	unsigned long int prime = 612852475143;
+	unsigned long int i = 3;
 
-  while (i <= prime / 2)
-    if (prime % i == 0)
-      prime = prime / i;
-    else
-      i += 2;
+	while (i <= prime / 2)
+		if (prime % i == 0)
+			prime = prime / i;
+		else
+			i += 2;
 
-  printf("%ld\n", prime);
+	printf("%ld\n", prime);
 
-  return (0);
+	return (0);
 }
