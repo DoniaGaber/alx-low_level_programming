dc8fbd5cb533b1ee02b41c0cbbfda41c3cc3c448
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-23 16:51:07 -0800

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,18 +10,18 @@
 
 void print_line(int n)
 {
-  int num_lines;
+	int num_lines;
 
-  if (n <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      for (num_lines = 1; num_lines <= n; num_lines++)
+	if (n <= 0)
 	{
-	  _putchar('_');
+		_putchar('\n');
 	}
-      _putchar('\n');
-    }
+	else
+	{
+		for (num_lines = 1; num_lines <= n; num_lines++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
