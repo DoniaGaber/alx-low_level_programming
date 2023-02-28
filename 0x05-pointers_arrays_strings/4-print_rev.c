44f91af3f6dceb043cac4075191be115fa94fe3d
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-02-27 16:16:39 -0800

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,15 @@
  */
 void print_rev(char *s)
 {
-  int n = 0;
-  int i, j;
+	int n = 0;
+	int i, j;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      n++;
-    }
-  for (j = (n - 1); j >= 0; j--)
-    {
-      _putchar(s[j]);
-    }
-  _putchar('\n');
-}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		n++;
+	}
+	for (j = (n - 1); j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
