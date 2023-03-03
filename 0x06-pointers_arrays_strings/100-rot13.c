a7d65d9d807a3ee797800633c813ec4bf28b8a3e
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-03-02 17:13:41 -0800

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,22 +8,22 @@
 
 char *rot13(char *s)
 {
-  int i, j;
-  char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char boolean;
+	int i, j;
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      boolean = 0;
-      for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	  if (s[i] == alpha[j])
-	    {
-	      s[i] = rot[j];
-	      boolean = 1;
-	    }
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+				boolean = 1;
+			}
+		}
 	}
-    }
-  return (s);
+	return (s);
 }
