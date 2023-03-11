d53947aaa231072bde6afb0f23565dc3b6623ae3
DoniaGaber <83475995+DoniaGaber@users.noreply.github.com>
2023-03-10 16:11:14 -0800

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,17 @@
-0;10;1c0;10;1c;il;
+#include <stdio.h>
+
+/**
+ ** main - prints its name, followed by a new line.
+ ** @argc: argument count
+ ** @argv: arguments
+ **
+ ** Return: 0
+ **/
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
